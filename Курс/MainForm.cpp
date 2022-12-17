9691a5505fce826d14cd3c6e112ae294ba4b8c09
taran.daniil03@mail.ru <taran.daniil03@mail.ru>
2022-12-17 19:14:25 +0700

    Добавьте файлы проекта.

---
+++ Курс/MainForm.cpp
@@ -0,0 +1,105 @@
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+int main(array<String^>^ arg) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Курс::MainForm form;
+	Application::Run(% form);
+}
+/*private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            draggedPiece = sender as Control;
+
+            if ((e.X <= resizingMargin) || (e.X >= draggedPiece.Width - resizingMargin) ||
+                (e.Y <= resizingMargin) || (e.Y >= draggedPiece.Height - resizingMargin))
+            {
+                resizing = true;
+
+                // indicate resizing
+                this.Cursor = Cursors.SizeNWSE;
+
+                // starting size
+                this.startSize = new Size(e.X, e.Y);
+                // get the location of the picture box
+                Point pt = this.PointToScreen(draggedPiece.Location);
+                rectProposedSize = new Rectangle(pt, startSize);
+                // draw rect
+                ControlPaint.DrawReversibleFrame(rectProposedSize, this.ForeColor, FrameStyle.Dashed);
+            }
+            else
+            {
+                resizing = false;
+                // indicate moving
+                this.Cursor = Cursors.SizeAll;
+            }
+
+            // start point location
+            this.startDraggingPoint = e.Location;
+        }
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+    }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draggedPiece != null)
+            {
+                if (resizing)
+                {
+                    // erase rect
+                    if (rectProposedSize.Width > 0 && rectProposedSize.Height > 0)
+                        ControlPaint.DrawReversibleFrame(rectProposedSize, this.ForeColor, FrameStyle.Dashed);
+                    // calculate rect new size
+                    rectProposedSize.Width = e.X - this.startDraggingPoint.X + this.startSize.Width;
+                    rectProposedSize.Height = e.Y - this.startDraggingPoint.Y + this.startSize.Height;
+                    // draw rect
+                    if (rectProposedSize.Width > 0 && rectProposedSize.Height > 0)
+                        ControlPaint.DrawReversibleFrame(rectProposedSize, this.ForeColor, FrameStyle.Dashed);
+                }
+                else
+                {
+                    Point pt;
+                    if (draggedPiece == sender)
+                        pt = new Point(e.X, e.Y);
+                    else
+                        pt = draggedPiece.PointToClient((sender as Control).PointToScreen(new Point(e.X, e.Y)));
+
+                    draggedPiece.Left += pt.X - this.startDraggingPoint.X;
+                    draggedPiece.Top += pt.Y - this.startDraggingPoint.Y;
+                }
+            }
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (resizing)
+            {
+                if (rectProposedSize.Width > 0 && rectProposedSize.Height > 0)
+                {
+                    // erase rect
+                    ControlPaint.DrawReversibleFrame(rectProposedSize, this.ForeColor, FrameStyle.Dashed);
+                }
+                // compare to min width and size ?
+                if (rectProposedSize.Width > 10 && rectProposedSize.Height > 10)
+                {
+                    // set size 
+                    this.draggedPiece.Size = rectProposedSize.Size;
+                }
+                else
+                {
+                    // you might want to set some minimal size here
+                    this.draggedPiece.Size = new Size((int)Math.Max(10, rectProposedSize.Width), Math.Max(10, rectProposedSize.Height));
+                }
+            }
+
+            this.draggedPiece = null;
+            this.startDraggingPoint = Point.Empty;
+            this.Cursor = Cursors.Default;
+        }*/
---
+++ Курс/MainForm.h
@@ -0,0 +1,106 @@
+#pragma once
+
+namespace  урс {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MainForm
+	/// </summary>
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	private:
+		Control draggedPiece = null;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+		   int creating_base = 0;
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(900, 50);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(97, 57);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(731, 221);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(130, 108);
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1009, 558);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MainForm";
+			this->Text = L"MainForm";
+			this->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &MainForm::MainForm_MouseDown);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		creating_base = 1;
+			this->Cursor = Cursors::SizeNWSE;
+	}
+	private: System::Void MainForm_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
+		if (creating_base) {
+
+		}
+	}
+	};
+}
